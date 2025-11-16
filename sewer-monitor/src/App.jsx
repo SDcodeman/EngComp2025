b3134a3bc13fc3ca5a6c6a976cbba08186d5594f
SDcodeman <121375981+SDcodeman@users.noreply.github.com>
2025-11-16 13:45:27 -0800

    With Routing

--- sewer-monitor/src/App.jsx
+++ sewer-monitor/src/App.jsx
@@ -1,16 +1,48 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/map">Map</Link>
+        <Link to="/list">List View</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/list" element={<List />} />
+        <Route path="*" element={<NotFound />} /> {/* 404 page */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return (
     <div className="app">
       <header className="header">
-        <h1>Sewer Monitor</h1>
+        <h1>SEWER VIEWER</h1>
       </header>
       <main className="main">
         <p>Welcome to Sewer Monitor</p>
       </main>
     </div>
   );
+}
+
+function Map() {
+  return <h1>Map Page</h1>;
+}
+
+function List() {
+  return <h1>List Page</h1>;
+}
+
+function NotFound() {
+  return <h1>404 - Page Not Found</h1>;
 }
 
 export default App;
